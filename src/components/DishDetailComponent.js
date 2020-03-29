6d8e58d162b131b7cdd264d84e4769f3fff57ddf
WenXinDong2018 <38565307+WenXinDong2018@users.noreply.github.com>
2020-03-29 13:47:13 -0700

    intro redux

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle , BreadcrumbItem, Breadcrumb} from 'reactstrap';
+import {Link} from "react-router-dom";
 class DishDetail extends Component{
 
     constructor(props){
@@ -12,17 +13,33 @@ class DishDetail extends Component{
             console.log("dish", this.props.dish)
 
             return (
-                <Card>
+                <div className = "container">
+                    <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    </div>
+                    <div className="col-12">
+                        <h3>{dish.name}</h3>
+                        <hr />
+                    </div>                
+                
+
+
+                    <Card>
                     <CardImg width = "100%" src={dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
-                        <p>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(this.props.dish.comments[0].date)))}</p>
+                        <p>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(this.props.comments[0].date)))}</p>
                     </CardBody>
                 </Card>
+                </div>
             )
         }else{
             return(
-                <div></div>
+                <div>hello</div>
             );
         }
     }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {Link} from "react-router-dom";
+
 const RenderMenuItem = (props) => {
     return(
-        <Card onClick = {() => props.onClick(props.dish.id)}>
+        <Card >
+                <Link to ={`/menu/${props.dish.id}`} >
                 <CardImg width = "100%" src={props.dish.image} alt={props.dish.name} />
-                  <CardImgOverlay>
-                    <CardTitle>{props.dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
+                <CardImgOverlay>
+                   <CardTitle>{props.dish.name}</CardTitle>
+                </CardImgOverlay>
+                </Link>
+        </Card>
     );
 }
 const Menu = (props) =>{
@@ -18,7 +22,7 @@ const Menu = (props) =>{
         const menu = props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish = {dish} onClick = {props.onClick}/>
+                <RenderMenuItem dish = {dish} />
               </div>
 
             );
@@ -27,7 +31,14 @@ const Menu = (props) =>{
         return (
             <div className = "container">
                 <div className = "row">
-                        {menu}
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to = "/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active> Menu </BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
+                <div className = "row">
+                    {menu}
                 </div>
             </div>
 
