97334330267ea4dc1416c0ef35e4a8fd4ba9f72b
kimchankyo <51008440+kimchankyo@users.noreply.github.com>
2020-04-19 17:33:11 -0700

    Updates

--- src/components/NotificationsPage.js
+++ src/components/NotificationsPage.js
@@ -1,7 +1,8 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem, Button, Badge} from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem, Button, Badge, Dropdown, 
+    DropdownToggle, DropdownMenu, DropdownItem, Nav, NavItem, NavLink} from 'reactstrap';
 import {Link} from "react-router-dom";
 
 //component for rendering a single notification
@@ -23,31 +24,84 @@ const RenderNotification = (props) => {
         </Card>
     );
 }
+
+const RenderReadNotification = (props) => {
+    return (
+        <Card body outline color = "secondary">
+            <CardBody>
+                <CardText>{props.notification.message_type}</CardText>
+                <CardText>Order Number: {props.notification.order_id}</CardText>
+                <CardText>Store: {props.notification.store}</CardText>
+                <CardText>Courier: {props.notification.courier}</CardText>
+                <CardText>
+                    <small className = "text-muted">Last updated {props.notification.last_update} minutes ago</small>
+                </CardText>
+                <Button color = "secondary">View Details</Button>
+            </CardBody>
+        </Card>
+    );
+}
+
 const NotificationsPage = (props) =>{
     
         
-        const menu = props.notifications.map((notification) => {
+        const unreadMenu = props.notifications.map((notification) => {
             return (
-                <div key={notification.id} className="col-12 col-md-6 m-1">
+                <div key={notification.id} className="col-12 col-md-5 m-1">
                 <RenderNotification notification = {notification} />
-                
                 </div>
 
             );
         });
+
+        const readMenu = props.notifications.map((notification) => {
+            return(
+                <div key={notification.id} className = "col-12 col-md-5 m-1">
+                <RenderReadNotification notification = {notification}/>
+                </div>
+            );
+        });
+        const [dropdownOpen, setDropdownOpen] = useState(false);
+  
+        const toggle = () => setDropdownOpen(prevState => !prevState);
 
         return (
             <div className = "container">
                 <div className = "row-md">
                     <br></br>
                     <h1>Notifications <Badge color="success">4 Updates</Badge></h1>
-                    <Button outline color="secondary">See Read Messages</Button>
-                    <br></br>
+                    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                        <DropdownToggle caret>
+                            Filter
+                        </DropdownToggle>
+                        <DropdownMenu>
+                        <DropdownItem>Unread Notifications</DropdownItem>
+                        <DropdownItem>Read Notifications</DropdownItem>
+                        <DropdownItem>All Notifications</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
                     <br></br>
                 </div>
+                <div>
+                    <Nav tabs>
+                        <NavItem>
+                            <NavLink href="#" active>
+                                General
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#">
+                                System Notifications
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </div>
+
                 <div className = "row">
-                    {menu}
-                    
+                    {unreadMenu}
+                </div>
+                <div className = "row">
+                    {readMenu}
                 </div>
             </div>
 
